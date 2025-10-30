19118678ca09579955912486c3d029e62e8bd778
raghavv619 <graghavv@outlook.com>
2025-10-30 21:33:05 +0530

    Auth added for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,16 @@
 import { desc } from "drizzle-orm";
 import { db } from "~/server/db/index"
+import { SignedOut, SignedIn } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
-
-export default async function HomePage() {
-  
+async function Images(){
   const images = await db.query.images.findMany({
     orderBy: (model, {desc}) => desc(model.id)
   })
 
   console.log(images);
-  
-  return (
-    <main className="">
+  return(
       <div className="flex flex-wrap gap-4">
         {[...images, ...images, ...images, ...images].map((image, index) => (
           <div key={image.id + "-" + index} className="flex flex-col w-48 ">
@@ -23,6 +20,19 @@ export default async function HomePage() {
         ))}
         {/* Hello, Gallery in Progress! */}
       </div>
+  )
+}
+
+export default async function HomePage() {
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="w-full h-full text-2xl text-center">Please Sign In!
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
