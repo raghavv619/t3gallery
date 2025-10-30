3f4c313ab3e48f4fd6b293b3707c169f3faf3da3
raghavv619 <graghavv@outlook.com>
2025-10-30 21:22:04 +0530

    Auth added!

---
+++ src/app/_components/TopNav.tsx
@@ -0,0 +1,21 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+
+export default function TopNav() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+      <div>Gallery</div>
+    <SignedOut><SignInButton /></SignedOut>
+    <SignedIn>
+        <UserButton />
+    </SignedIn>
+      {/* <div>Sign In</div> */}
+    </nav>
+  );
+}
