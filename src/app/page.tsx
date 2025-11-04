f582eea0c4fd618e9d4cd97401ab4f2456b1cc6e
raghavv619 <graghavv@outlook.com>
2025-11-04 19:12:21 +0530

    Adding padding at home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ const images = await getMyImages();
   console.log(images);
   return(
       <div className="flex flex-wrap justify-center gap-4 p-4">
-        {[...images, ...images, ...images, ...images].map((image) => (
+        {images.map((image) => (
           <div key={image.id} className="flex justify-center flex-col w-48 h-48">
             <Link href={`/img/${image.id}`}>
             <Image src={image.url} alt={image.name} style={{objectFit:"contain"}}width={192}
